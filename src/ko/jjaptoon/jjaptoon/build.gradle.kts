f3f7af6cf8187cea251aa543bf43d13cd81a9e55
insid0909-max <insid0909@gmail.com>
2026-06-30 10:05:01 +0900

    Fix: use build.gradle.kts format

---
+++ src/ko/jjaptoon/jjaptoon/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+        alias(kei.plugins.extension)
+}
+
+keiyoushi {
+        name = "Jjaptoon"
+            versionCode = 1
+                contentWarning = keiyoushi.ContentWarning.PORNOGRAPHIC
+                    libVersion = "1.4"
+
+                        source {
+                                    name = "짭툰"
+                                            lang = "ko"
+                                                    baseUrl = "https://www.jjaptoon003.com"
+                        }
+}
+                        }
+}
+}
